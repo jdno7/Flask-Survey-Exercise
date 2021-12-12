5bc3393176088f64a51cb863d3d29122630242b3
john hans <jdhans34@gmail.com>
2021-12-12 09:21:45 -0600

    session complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,31 +7,40 @@ app = Flask(__name__)
 answers = 0
 
 app.config['SECRET_KEY'] = 'oh-so-secret'
-
+#test
 debug = DebugToolbarExtension(app)
 
-responses = []
+responses = 'responses'
 
 @app.route('/')
 def home():
-    question_num = len(responses)
+    session[responses] = []
+    # session.permanent = True
+    # responses = session['responses']
+    question_num = 0
     title = satisfaction_survey.title
     instructions = satisfaction_survey.instructions
     return render_template('home.html',title=title,instructions=instructions,question_num=question_num)
 
 @app.route('/questions/<question_num>')
 def questions(question_num):
-    if request.args:
-        responses.append(request.args['answer'])
-
+    responses = session['responses']
+    question_num = len(responses)
     
+    if request.args:
+        responses = session['responses']
+        answer = request.args['answer']
+        responses.append(answer)
+        session['responses'] = responses
+        
     
     if len(responses) == len(satisfaction_survey.questions):
+            responses = []
             return render_template('thankyou.html')
-
+    
     question_num = len(responses)
     question = satisfaction_survey.questions[question_num].question
     choose = satisfaction_survey.questions[question_num].choices
-
-    return render_template('questions.html',question_num=question_num,question=question,choose=choose,responses=responses)
+    
+    return render_template('questions.html',question_num=question_num,question=question,choose=choose,)
     
